941a82df0376687a48d6bb9b9f6477a7a7bee297
J3alou5 <karliskade@gmail.com>
2024-09-27 10:22:28 +0300

    new

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, Image, ScrollView } from "react-native";
+import { Text, View } from 'react-native';
+import { Link } from "expo-router";
+
 
 export default function App(){
     return (
-        <View style={StyleSheet.container}>
-            <Text>Kade</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-3xl">Kade</Text>
             <StatusBar style="auto"/>
+            <Link href="/profile" style={{color: 'blue'}}> go to profile</Link>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
