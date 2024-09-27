3502114c625c989fd7dbc1d2bc255197aeec4d65
J3alou5 <karlis.kade@voco.ee>
2024-09-27 10:37:43 +0300

    setup-done

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ import { Link } from "expo-router";
 export default function App(){
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl">Kade</Text>
+            <Text className="text-3xl font-pblack">Kade</Text>
             <StatusBar style="auto"/>
             <Link href="/profile" style={{color: 'blue'}}> go to profile</Link>
         </View>
